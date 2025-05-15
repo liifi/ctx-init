34ab5e1044692b1ee0e39c202f9b0dc5545c9eb9
Julio Stanley <juliostanley@users.noreply.github.com>
2025-05-15 12:23:02 -0400

    quick fix

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// main.ts
 import { parse as parseFlags } from "jsr:@std/flags";
 import {
 	SecretsManagerClient, GetSecretValueCommand,
@@ -46,7 +47,7 @@ function parseCommandArgs(command: string): string[] {
 
 
 
-async function runCommand(args: string[], forwardSignals = false): Promise<Deno.ProcessStatus> {
+async function runCommand(args: string[], forwardSignals = false): Promise<Deno.CommandStatus> {
 	if (args.length === 0) {
 		console.debug("No command to run.");
 		return { success: true, code: 0, signal: null };
@@ -75,7 +76,7 @@ async function runCommand(args: string[], forwardSignals = false): Promise<Deno.
 			try {
 				process.kill(sig);
 			} catch (e) {
-				(logger || console).error(`Failed to forward signal ${sig} to PID ${process.pid}`, e);
+				console.error(`Failed to forward signal ${sig} to PID ${process.pid}`, e);
 			}
 		};
 
@@ -105,11 +106,17 @@ async function runCommand(args: string[], forwardSignals = false): Promise<Deno.
 	return status;
 }
 
-function isSuppressedError(status: Deno.ProcessStatus): boolean {
+function isSuppressedError(status: Deno.CommandStatus): boolean {
 	if (status.success) return true; // Exited with status 0
-	if (status.signal === "SIGINT" || status.signal === "SIGTERM" || status.signal === "SIGKILL") {
-		return true;
-	}
+  if (status.signal === undefined){
+    if (status.code === 2 || status.code === 15 || status.code === 9) {
+      return true;
+    }
+  } else {
+    if ((status.signal as string) === "SIGINT" || (status.signal as string) === "SIGTERM" || (status.signal as string) === "SIGKILL") {
+      return true;
+    }
+  }
 	return false;
 }
 
@@ -263,3 +270,70 @@ if (import.meta.main) {
 		Deno.exit(127); // General error code
 	});
 }
+
+
+/*
+Codes
+1 SIGHUP
+2 SIGINT
+3 SIGQUIT
+4 SIGILL
+5 SIGTRAP
+6 SIGABRT
+7 SIGBUS
+8 SIGFPE
+9 SIGKILL
+10 SIGUSR1
+11 SIGSEGV
+12 SIGUSR2
+13 SIGPIPE
+14 SIGALRM
+15 SIGTERM
+16 SIGSTKFLT
+17 SIGCHLD
+18 SIGCONT
+19 SIGSTOP
+20 SIGTSTP
+21 SIGTTIN
+22 SIGTTOU
+23 SIGURG
+24 SIGXCPU
+25 SIGXFSZ
+26 SIGVTALRM
+27 SIGPROF
+28 SIGWINCH
+29 SIGIO
+30 SIGPWR
+31 SIGSYS
+34 SIGRTMIN
+35 SIGRTMIN+1
+36 SIGRTMIN+2
+37 SIGRTMIN+3
+38 SIGRTMIN+4
+39 SIGRTMIN+5
+40 SIGRTMIN+6
+41 SIGRTMIN+7
+42 SIGRTMIN+8
+43 SIGRTMIN+9
+44 SIGRTMIN+10
+45 SIGRTMIN+11
+46 SIGRTMIN+12
+47 SIGRTMIN+13
+48 SIGRTMIN+14
+49 SIGRTMIN+15
+50 SIGRTMAX-14
+51 SIGRTMAX-13
+52 SIGRTMAX-12
+53 SIGRTMAX-11
+54 SIGRTMAX-10
+55 SIGRTMAX-9
+56 SIGRTMAX-8
+57 SIGRTMAX-7
+58 SIGRTMAX-6
+59 SIGRTMAX-5
+60 SIGRTMAX-4
+61 SIGRTMAX-3
+62 SIGRTMAX-2
+63 SIGRTMAX-1
+64 SIGRTMAX
+*/
